a5859de63f3f31e7b471c90229c87f365dabc6fc
youyo <1003ni2@gmail.com>
2015-03-09 18:59:36 +0900

    Add Code coverage.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,10 @@
 require 'rspec'
 require 'any/validate'
+require "codeclimate-test-reporter"
+require 'simplecov'
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[SimpleCov::Formatter::HTMLFormatter,CodeClimate::TestReporter::Formatter]
+SimpleCov.start CodeClimate::TestReporter.configuration.profile
 
 RSpec.configure do |config|
   config.include Any::Validate
